d31d5ca43e8c28316dbfc34b438331bccab47924
sifatullahsu <personal.sifat@gmail.com>
2022-10-25 16:06:35 +0600

    Courses Data

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
   res.send('Learn Villa server is running...');
+});
+
+app.get('/courses', (req, res) => {
+  res.send(courses);
 });
 
 
